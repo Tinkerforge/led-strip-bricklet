7cd5d3cffc7034be5d18108003130f478fc09402
Matthias Bolte <matthias@tinkerforge.com>
2013-12-19 18:38:46 +0100

    php: Cleanup example

--- software/examples/php/ExampleCallback.php
+++ software/examples/php/ExampleCallback.php
@@ -6,10 +6,11 @@ require_once('Tinkerforge/BrickletLEDStrip.php');
 use Tinkerforge\IPConnection;
 use Tinkerforge\BrickletLEDStrip;
 
-$host = 'localhost';
-$port = 4223;
-$uid = 'XYZ'; // Change to your UID
-$NUM_LEDS = 16;
+const HOST = 'localhost';
+const PORT = 4223;
+const UID = 'XYZ'; // Change to your UID
+const NUM_LEDS = 16;
+
 $r = array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 $g = array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 $b = array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
@@ -17,34 +18,37 @@ $rIndex = 0;
 
 // Frame rendered callback, is called when a new frame was rendered
 // We increase the index of one blue LED with every frame
-function cb_frame_rendered($length)
+function cb_frameRendered($length)
 {
-	global $NUM_LEDS, $rIndex, $r, $g, $b, $ledStrip;
-	$b[$rIndex] = 0;
-	if($rIndex == $NUM_LEDS-1) {
-		$rIndex = 0;
-	} else {
-		$rIndex++;
-	}
-	$b[$rIndex] = 255;
+    global $rIndex, $r, $g, $b, $ledStrip;
 
-	$ledStrip->setRGBValues(0, $NUM_LEDS, $r, $g, $b);
+    $b[$rIndex] = 0;
+
+    if ($rIndex == NUM_LEDS - 1) {
+        $rIndex = 0;
+    } else {
+        $rIndex++;
+    }
+
+    $b[$rIndex] = 255;
+
+    $ledStrip->setRGBValues(0, NUM_LEDS, $r, $g, $b);
 }
 
 $ipcon = new IPConnection(); // Create IP connection
-$ledStrip = new BrickletLEDStrip($uid, $ipcon); // Create device object
+$ledStrip = new BrickletLEDStrip(UID, $ipcon); // Create device object
 
-$ipcon->connect($host, $port); // Connect to brickd
+$ipcon->connect(HOST, PORT); // Connect to brickd
 // Don't use device before ipcon is connected
 
 // Set frame duration to 50ms (20 frames per second)
 $ledStrip->setFrameDuration(50);
 
 // Register frame rendered callback to function cb_frame_rendered
-$ledStrip->registerCallback(BrickletLEDStrip::CALLBACK_FRAME_RENDERED, 'cb_frame_rendered');
+$ledStrip->registerCallback(BrickletLEDStrip::CALLBACK_FRAME_RENDERED, 'cb_frameRendered');
 
 // Set initial rgb values to get started
-$ledStrip->setRGBValues(0, $NUM_LEDS, $r, $g, $b);
+$ledStrip->setRGBValues(0, NUM_LEDS, $r, $g, $b);
 
 echo "Press ctrl+c to exit\n";
 $ipcon->dispatchCallbacks(-1); // Dispatch callbacks forever
--- software/examples/php/ExampleSimple.php
+++ software/examples/php/ExampleSimple.php
@@ -6,14 +6,14 @@ require_once('Tinkerforge/BrickletLEDStrip.php');
 use Tinkerforge\IPConnection;
 use Tinkerforge\BrickletLEDStrip;
 
-$host = 'localhost';
-$port = 4223;
-$uid = 'XYZ'; // Change to your UID
+const HOST = 'localhost';
+const PORT = 4223;
+const UID = 'XYZ'; // Change to your UID
 
 $ipcon = new IPConnection(); // Create IP connection
-$ledStrip = new BrickletLEDStrip($uid, $ipcon); // Create device object
+$ledStrip = new BrickletLEDStrip(UID, $ipcon); // Create device object
 
-$ipcon->connect($host, $port); // Connect to brickd
+$ipcon->connect(HOST, PORT); // Connect to brickd
 // Don't use device before ipcon is connected
 
 // Set first 10 LEDs to green
