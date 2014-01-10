f1bb0616d167e72acf47a400f3d756f9b3a1e11b
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-01-10 15:55:32 +0100

    Add Perl examples

---
+++ software/examples/perl/example_callback.pl
@@ -0,0 +1,59 @@
+#!/usr/bin/perl  
+
+use Tinkerforge::IPConnection;
+use Tinkerforge::BrickletLEDStrip;
+
+use constant HOST => 'localhost';
+use constant PORT => 4223;
+use constant UID => 'oud'; # Change to your UID
+
+use constant NUM_LEDS => 16;
+
+our $r = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+our $g = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+our $b = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+our $r_index = 0;
+
+my $ipcon = IPConnection->new(); # Create IP connection
+my $led_strip = BrickletLEDStrip->new(&UID, $ipcon); # Create device object
+
+# Frame rendered callback, is called when a new frame was rendered
+# We increase the index of one blue LED with every frame
+sub cb_frame_rendered
+{
+    my ($length) = @_;
+
+    if ($r_index == NUM_LEDS)
+    {
+        $r_index = 0;
+
+        $r = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        $g = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        $b = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    }
+    else
+    {
+        $r_index += 1;
+    }
+    @{$b}[$r_index] = 255;
+
+    # Set new data for next render cycle
+    $led_strip->set_rgb_values(0, NUM_LEDS, $r, $g, $b);
+}
+
+$ipcon->connect(&HOST, &PORT); # Connect to brickd
+# Don't use device before ipcon is connected
+
+# Set frame duration to 50ms (20 frames per second)
+$led_strip->set_frame_duration(50);
+
+# Register frame rendered callback to function cb_frame_rendered
+$led_strip->register_callback($led_strip->CALLBACK_FRAME_RENDERED, 'cb_frame_rendered');
+
+# Set initial rgb values to get started
+$led_strip->set_rgb_values(0, NUM_LEDS, $r, $g, $b);
+
+print "\nPress any key to exit...\n";
+<STDIN>;
+$ipcon->disconnect();
+
---
+++ software/examples/perl/example_simple.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl  
+
+use Tinkerforge::IPConnection;
+use Tinkerforge::BrickletLEDStrip;
+
+use constant HOST => 'localhost';
+use constant PORT => 4223;
+use constant UID => 'oud'; # Change to your UID
+
+my $ipcon = IPConnection->new(); # Create IP connection
+my $led_strip = BrickletLEDStrip->new(&UID, $ipcon); # Create device object
+
+$ipcon->connect(&HOST, &PORT); # Connect to brickd
+# Don't use device before ipcon is connected
+
+# Set first 10 LEDs to green
+my $r = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+my $g = [255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 0, 0, 0, 0, 0, 0];
+my $b = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+$led_strip->set_rgb_values(0, 10, $r, $g, $b);
+
+print "\nPress any key to exit...\n";
+<STDIN>;
+$ipcon->disconnect();
+
