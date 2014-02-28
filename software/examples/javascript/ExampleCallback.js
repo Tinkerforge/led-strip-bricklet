edbe25c42163129dcc48cf44a9fb7f31aa8f7693
Matthias Bolte <matthias@tinkerforge.com>
2014-02-28 19:19:30 +0100

    javascript: Break long lines and cleanup whitespace

--- software/examples/javascript/ExampleCallback.js
+++ software/examples/javascript/ExampleCallback.js
@@ -2,7 +2,7 @@ var Tinkerforge = require('tinkerforge');
 
 var HOST = 'localhost';
 var PORT = 4223;
-var UID = 'oud';// Change to your UID
+var UID = 'oud'; // Change to your UID
 
 var NUM_LEDS = 16;
 var r = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
@@ -10,29 +10,29 @@ var g = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 var b = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 var rIndex = 0;
 
-var ipcon = new Tinkerforge.IPConnection();// Create IP connection
-var ls = new Tinkerforge.BrickletLEDStrip(UID, ipcon);// Create device object
+var ipcon = new Tinkerforge.IPConnection(); // Create IP connection
+var ls = new Tinkerforge.BrickletLEDStrip(UID, ipcon); // Create device object
 
 ipcon.connect(HOST, PORT,
     function(error) {
-        console.log('Error: '+error);        
+        console.log('Error: '+error);
     }
-);// Connect to brickd
-
+); // Connect to brickd
 // Don't use device before ipcon is connected
+
 ipcon.on(Tinkerforge.IPConnection.CALLBACK_CONNECTED,
     function(connectReason) {
-        //Set frame duration to 50ms (20 frames per second)
-            ls.setFrameDuration(50);
-        //Set initial rgb values to get started
-            ls.setRGBValues(0, NUM_LEDS, r, g, b);
+        // Set frame duration to 50ms (20 frames per second)
+        ls.setFrameDuration(50);
+        // Set initial rgb values to get started
+        ls.setRGBValues(0, NUM_LEDS, r, g, b);
     }
 );
 
 // Register frame rendered callback
 ls.on(Tinkerforge.BrickletLEDStrip.CALLBACK_FRAME_RENDERED,
-    //Frame rendered callback, is called when a new frame was rendered
-    //We increase the index of one blue LED with every frame
+    // Frame rendered callback, is called when a new frame was rendered
+    // We increase the index of one blue LED with every frame
     function(len) {
         b[rIndex] = 0;
         if(rIndex === NUM_LEDS-1) {
@@ -42,7 +42,7 @@ ls.on(Tinkerforge.BrickletLEDStrip.CALLBACK_FRAME_RENDERED,
             rIndex += 1;
         }
         b[rIndex] = 255;
-        //Set new data for next render cycle
+        // Set new data for next render cycle
         ls.setRGBValues(0, NUM_LEDS, r, g, b)
     }
 );
@@ -54,4 +54,3 @@ process.stdin.on('data',
         process.exit(0);
     }
 );
-
