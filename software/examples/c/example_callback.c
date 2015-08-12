f523c45acbc9a76aa729db2dddd949a311897af0
Matthias Bolte <matthias@tinkerforge.com>
2015-08-12 11:26:27 +0200

    Fix minor warnings in C/C++ examples

--- software/examples/c/example_callback.c
+++ software/examples/c/example_callback.c
@@ -33,7 +33,7 @@ void cb_frame_rendered(uint16_t length, void *user_data) {
 	led_strip_set_rgb_values(ls, 0, NUM_LEDS, r, g, b);
 }
 
-int main() {
+int main(void) {
 	// Create IP connection
 	IPConnection ipcon;
 	ipcon_create(&ipcon);
@@ -45,7 +45,7 @@ int main() {
 	// Connect to brickd
 	if(ipcon_connect(&ipcon, HOST, PORT) < 0) {
 		fprintf(stderr, "Could not connect\n");
-		exit(1);
+		return 1;
 	}
 	// Don't use device before ipcon is connected
 	
@@ -64,4 +64,5 @@ int main() {
 	printf("Press key to exit\n");
 	getchar();
 	ipcon_destroy(&ipcon); // Calls ipcon_disconnect internally
+	return 0;
 }
