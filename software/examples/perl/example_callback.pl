220b64771e5cd7adbd6c78b1ce1e3797a98247b5
Matthias Bolte <matthias@tinkerforge.com>
2015-08-19 16:02:04 +0200

    Cleanup Perl examples

--- software/examples/perl/example_callback.pl
+++ software/examples/perl/example_callback.pl
@@ -1,12 +1,11 @@
-#!/usr/bin/perl  
+#!/usr/bin/perl
 
 use Tinkerforge::IPConnection;
 use Tinkerforge::BrickletLEDStrip;
 
 use constant HOST => 'localhost';
 use constant PORT => 4223;
-use constant UID => 'jEE'; # Change to your UID
-
+use constant UID => 'XYZ'; # Change to your UID
 use constant NUM_LEDS => 16;
 
 our $r = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
@@ -15,7 +14,7 @@ our $b = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 our $r_index = 0;
 
 my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
-our $led_strip = Tinkerforge::BrickletLEDStrip->new(&UID, $ipcon); # Create device object
+our $ls = Tinkerforge::BrickletLEDStrip->new(&UID, $ipcon); # Create device object
 
 # Frame rendered callback, is called when a new frame was rendered
 # We increase the index of one blue LED with every frame
@@ -37,22 +36,21 @@ sub cb_frame_rendered
     @{$b}[$r_index] = 255;
 
     # Set new data for next render cycle
-    $led_strip->set_rgb_values(0, NUM_LEDS, $r, $g, $b);
+    $ls->set_rgb_values(0, NUM_LEDS, $r, $g, $b);
 }
 
 $ipcon->connect(&HOST, &PORT); # Connect to brickd
 # Don't use device before ipcon is connected
 
 # Set frame duration to 50ms (20 frames per second)
-$led_strip->set_frame_duration(50);
+$ls->set_frame_duration(50);
 
 # Register frame rendered callback to function cb_frame_rendered
-$led_strip->register_callback($led_strip->CALLBACK_FRAME_RENDERED, 'cb_frame_rendered');
+$ls->register_callback($ls->CALLBACK_FRAME_RENDERED, 'cb_frame_rendered');
 
 # Set initial rgb values to get started
-$led_strip->set_rgb_values(0, NUM_LEDS, $r, $g, $b);
+$ls->set_rgb_values(0, NUM_LEDS, $r, $g, $b);
 
 print "Press any key to exit...\n";
 <STDIN>;
 $ipcon->disconnect();
-
--- software/examples/perl/example_simple.pl
+++ software/examples/perl/example_simple.pl
@@ -1,14 +1,14 @@
-#!/usr/bin/perl  
+#!/usr/bin/perl
 
 use Tinkerforge::IPConnection;
 use Tinkerforge::BrickletLEDStrip;
 
 use constant HOST => 'localhost';
 use constant PORT => 4223;
-use constant UID => 'oud'; # Change to your UID
+use constant UID => 'XYZ'; # Change to your UID
 
 my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
-my $led_strip = Tinkerforge::BrickletLEDStrip->new(&UID, $ipcon); # Create device object
+my $ls = Tinkerforge::BrickletLEDStrip->new(&UID, $ipcon); # Create device object
 
 $ipcon->connect(&HOST, &PORT); # Connect to brickd
 # Don't use device before ipcon is connected
@@ -18,9 +18,8 @@ my $r = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 my $g = [255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 0, 0, 0, 0, 0, 0];
 my $b = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-$led_strip->set_rgb_values(0, 10, $r, $g, $b);
+$ls->set_rgb_values(0, 10, $r, $g, $b);
 
 print "Press any key to exit...\n";
 <STDIN>;
 $ipcon->disconnect();
-
