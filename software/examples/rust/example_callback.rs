09c834c61e651cf88941f02bbef8888b8e2b797d
Matthias Bolte <matthias@tinkerforge.com>
2020-03-16 17:41:09 +0100

    rust: Update to new skeleton

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -1,17 +1,17 @@
 use std::{error::Error, io, thread};
 use tinkerforge::{ip_connection::IpConnection, led_strip_bricklet::*};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your LED Strip Bricklet
+const UID: &str = "XYZ"; // Change XYZ to the UID of your LED Strip Bricklet.
 const NUM_LEDS: u8 = 16;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let ls = LedStripBricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let ls = LedStripBricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd
-                                          // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
     // Set frame duration to 50ms (20 frames per second)
     ls.set_frame_duration(50);
@@ -23,7 +23,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut b = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
     ls.set_rgb_values(0, NUM_LEDS, r, g, b);
 
-    // Spawn thread to handle received events.
+    // Spawn thread to handle received callback messages.
     // This thread ends when the `ls` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
